425e7a4ba087749faa15b61e7d7ed587d1b8af50
EPLJVKHPSP <94285566+EPLJVKHPSP@users.noreply.github.com>
2023-07-08 13:42:41 +0300

    Calculator v2.0
    Changes:
    
    Transitioned from terminal-based application to a Telegram bot interface.
    The bot fetches real-time cryptocurrency prices from CoinGecko API.
    Implemented the conversational interface with several stages for data input (tokens before and after, and commission).
    Provided options to go back or cancel the operation at any stage.
    The bot calculates the impermanent loss (IL) based on the input data.
    Description:
    
    This version represents a major overhaul from the previous terminal-based application to a Telegram bot. The bot is designed to interact with users in a conversational style to calculate the impermanent loss for cryptocurrency liquidity provisioning.
    
    Users enter the tokens' names and quantities before and after provisioning, as well as the farmed commission. The bot then calculates the impermanent loss based on real-time cryptocurrency prices fetched from the CoinGecko API. It also allows users to cancel the operation or navigate back at any stage.
    
    Please note: For the security of the application, do not share the bot token publicly, as it's hardcoded in the main function. You might want to consider setting it through environment variables or a secure configuration file.

--- calculator.py
+++ calculator.py
@@ -1,44 +1,140 @@
+import os
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
 from pycoingecko import CoinGeckoAPI
 
 def get_price(crypto_name):
     cg_client = CoinGeckoAPI()
-    data = cg_client.get_price(ids = crypto_name, vs_currencies = 'usd')
-    return data[crypto_name]['usd']
+    try:
+        data = cg_client.get_price(ids=crypto_name, vs_currencies='usd')
+        return data[crypto_name]['usd']
+    except Exception as e:
+        print(f"Failed to get price: {e}")
+        return None
 
-def calculate_il():
-    # Get inputs
-    crypto1_name = input("Enter the name of the first cryptocurrency: ")
-    crypto1_qty_before = float(input(f"Enter the quantity of {crypto1_name} before: "))
-    crypto1_qty_after = float(input(f"Enter the quantity of {crypto1_name} after: "))
+CRYPTO1, CRYPTO1_BEFORE, CRYPTO1_AFTER, CRYPTO2, CRYPTO2_BEFORE, CRYPTO2_AFTER, CRYPTO2_FINAL_QTY, COMMISSION = range(8)
 
-    crypto2_name = input("Enter the name of the second cryptocurrency: ")
-    crypto2_qty_before = float(input(f"Enter the quantity of {crypto2_name} before: "))
-    crypto2_qty_after = float(input(f"Enter the quantity of {crypto2_name} after: "))
+def send_menu(update: Update, context: CallbackContext) -> int:
+    keyboard = [['IL Calc'], ['Back']]
+    reply_markup = ReplyKeyboardMarkup(keyboard)
+    update.message.reply_text('Choose an option in menu, all the token names you can find on coingecko.com', reply_markup=reply_markup)
+    return CRYPTO1
 
-    while True:
-        try:
-            commission = float(input("Enter the commission: "))
-            break
-        except ValueError:
-            print("Invalid input. Please enter a numeric value for the commission.")
+def il_calc(update: Update, context: CallbackContext) -> int:
+    if update.message.text == 'IL Calc':
+        update.message.reply_text("Token1 Name?")
+        return CRYPTO1_BEFORE
+    elif update.message.text == 'Back':
+        return send_menu(update, context)
+    return send_menu(update, context)
 
-    # Calculate impermanent loss
-    if crypto1_qty_before > crypto1_qty_after:
-        changed_crypto_qty = crypto1_qty_before - crypto1_qty_after
-        changed_crypto_price = get_price(crypto1_name)
-        changed_amount_usd = changed_crypto_qty * changed_crypto_price
-        crypto2_price = get_price(crypto2_name)
-        changed_amount_crypto2 = changed_amount_usd / crypto2_price
-        il = (crypto2_qty_after - crypto2_qty_before - changed_amount_crypto2) * crypto2_price + commission
-    else:
-        changed_crypto_qty = crypto2_qty_before - crypto2_qty_after
-        changed_crypto_price = get_price(crypto2_name)
-        changed_amount_usd = changed_crypto_qty * changed_crypto_price
-        crypto1_price = get_price(crypto1_name)
-        changed_amount_crypto1 = changed_amount_usd / crypto1_price
-        il = (crypto1_qty_after - crypto1_qty_before - changed_amount_crypto1) * crypto1_price + commission
+def crypto1_before(update: Update, context: CallbackContext) -> int:
+    if update.message.text == 'Back':
+        return send_menu(update, context)
+    context.user_data['crypto1_name'] = update.message.text
+    update.message.reply_text(f"Quantity of {context.user_data['crypto1_name']} BEFORE?")
+    return CRYPTO1_AFTER
 
-    return round(il, 2)
+def crypto1_after(update: Update, context: CallbackContext) -> int:
+    try:
+        context.user_data['crypto1_qty_before'] = float(update.message.text)
+        update.message.reply_text(f"Quantity of {context.user_data['crypto1_name']} AFTER?:")
+        return CRYPTO2
+    except ValueError:
+        update.message.reply_text(f"Invalid input. Please enter a number for the quantity of {context.user_data['crypto1_name']} before:")
+        return CRYPTO1_AFTER
 
-il = calculate_il()
-print(f"The impermanent loss is: {il}$")
+def crypto2(update: Update, context: CallbackContext) -> int:
+    if update.message.text == 'Back':
+        return send_menu(update, context)
+    context.user_data['crypto1_qty_after'] = float(update.message.text)
+    update.message.reply_text("Token2 Name?")
+    return CRYPTO2_BEFORE
+
+def crypto2_before(update: Update, context: CallbackContext) -> int:
+    if update.message.text == 'Back':
+        return send_menu(update, context)
+    context.user_data['crypto2_name'] = update.message.text
+    update.message.reply_text(f"Quantity of {context.user_data['crypto2_name']} BEFORE?")
+    return CRYPTO2_AFTER
+
+def crypto2_after(update: Update, context: CallbackContext) -> int:
+    try:
+        context.user_data['crypto2_qty_before'] = float(update.message.text)
+        update.message.reply_text(f"Quantity of {context.user_data['crypto2_name']} AFTER?")
+        return CRYPTO2_FINAL_QTY
+    except ValueError:
+        update.message.reply_text(f"Invalid input. Please enter a number for the quantity of {context.user_data['crypto2_name']} before:")
+        return CRYPTO2_AFTER
+
+def crypto2_final_qty(update: Update, context: CallbackContext) -> int:
+    try:
+        context.user_data['crypto2_qty_after'] = float(update.message.text)
+        update.message.reply_text("Farmed comission?")
+        return COMMISSION
+    except ValueError:
+        update.message.reply_text(f"Invalid input. Please enter a number for the quantity of {context.user_data['crypto2_name']} after:")
+        return CRYPTO2_AFTER
+
+def commission(update: Update, context: CallbackContext) -> int:
+    try:
+        context.user_data['commission'] = float(update.message.text)
+        return calculate_il(update, context)   # move to calculate_il after setting the commission
+    except ValueError:
+        update.message.reply_text("Invalid input. Please enter a number for the commission:")
+        return COMMISSION
+
+def calculate_il(update: Update, context: CallbackContext) -> int:
+    try:
+        crypto1_qty_before = context.user_data['crypto1_qty_before']
+        crypto1_qty_after = context.user_data['crypto1_qty_after']
+        crypto2_qty_before = context.user_data['crypto2_qty_before']
+        crypto2_qty_after = context.user_data['crypto2_qty_after']
+        commission = context.user_data['commission']
+
+        crypto1_name = context.user_data['crypto1_name']
+        crypto2_name = context.user_data['crypto2_name']
+
+        price1_after = get_price(crypto1_name)
+        price2_after = get_price(crypto2_name)
+
+        il = ((crypto2_qty_after - crypto2_qty_before - (crypto1_qty_before - crypto1_qty_after) * price1_after / price2_after)) * price2_after + commission
+
+        update.message.reply_text(f"The impermanent loss is: {round(il, 2)}$")
+        return send_menu(update, context)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return ConversationHandler.END
+
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text("Operation cancelled.")
+    return ConversationHandler.END
+
+def main():
+    bot_token = 'TELEGRAM-API'
+    updater = Updater(token=bot_token, use_context=True)
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', send_menu)],
+        states={
+            CRYPTO1: [MessageHandler(Filters.text & ~Filters.command, il_calc)],
+            CRYPTO1_BEFORE: [MessageHandler(Filters.text & ~Filters.command, crypto1_before)],
+            CRYPTO1_AFTER: [MessageHandler(Filters.text & ~Filters.command, crypto1_after)],
+            CRYPTO2: [MessageHandler(Filters.text & ~Filters.command, crypto2)],
+            CRYPTO2_BEFORE: [MessageHandler(Filters.text & ~Filters.command, crypto2_before)],
+            CRYPTO2_AFTER: [MessageHandler(Filters.text & ~Filters.command, crypto2_after)],
+            CRYPTO2_FINAL_QTY: [MessageHandler(Filters.text & ~Filters.command, crypto2_final_qty)],
+            COMMISSION: [MessageHandler(Filters.text & ~Filters.command, commission)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    updater.dispatcher.add_handler(conv_handler)
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+
